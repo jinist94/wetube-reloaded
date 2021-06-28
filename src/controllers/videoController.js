e31053fb5737b18ddebd6e874a29eafaca80a602
jinist94 <jay@Hamui-MacBook-Pro.local>
2021-06-29 02:15:41 +0900

    0629

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,49 @@
-const fakeUser = {
-    username: "sojin",
-    logged: false,
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 3,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 3,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 3,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 3,
+    },
+
+];
+
+export const tranding = (req, res) => {
+    return res.render("home", { pageTitle: "Home", videos });
+}
+export const watch = (req, res) => {
+    const { id } = req.params; // same thing > const id = req.params.id;
+    const video = videos[id - 1];
+    return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
 
-export const tranding = (req, res) =>
-    res.render("home", { pageTitle: "Home", fakeUser: "fakeUser"});
-
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-    return res.send(`Delete Video #${req.params.id}`);
+export const getEdit = (req, res) => {
+    const { id } = req.params; // same thing > const id = req.params.id;
+    const video = videos[id - 1];
+    return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body; // const title = req.body.title;
+    videos[id - 1].title = title;
+    return res.redirect(`/videos/${id}`);
+}
+
